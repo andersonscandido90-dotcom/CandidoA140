ddf07d1d9ae9855959548eb4a55a34e11a5b9e29
andersonscandido90-dotcom <andersonscandido90@gmail.com>
2026-01-07 23:07:58 -0300

    Remove capacity from atraves in SECTIONS
    Removed capacity property from atraves interface in SECTIONS.

--- components/CAVPanel.tsx
+++ components/CAVPanel.tsx
@@ -29,7 +29,6 @@ interface Eductor {
   side?: 'BB' | 'BE';
 }
 interface atraves {
-  capacity: number;
   local: string;
 }
 
@@ -40,7 +39,7 @@ interface SectionData {
 }
 
 const SECTIONS: SectionData[] = [
-  { section: 'C', atraves: [{ capacity: 15, local: 'Via compartimento 9D'}]},
+  { section: 'C', atraves: [{ local: 'Via compartimento 9D'}]},
   { section: 'D', eductors: [{ capacity: 15, deck: 9 }] },
   { section: 'F', eductors: [{ capacity: 15, deck: 9 }] },
   { section: 'G', eductors: [{ capacity: 75, deck: 9, side: 'BB' }, { capacity: 75, deck: 9, side: 'BE' }] },
